c97e09ab1834fdfca0636835c35bb34c823f0ab5
jmcc-137 <jmcrispinc20@gmail.com>
2025-05-21 11:02:05 -0500

    feat: :sparkles: se modifico la barra de inicio(reponsive)

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.getElementById('hamburger-btn');
   const navMenu = document.querySelector('.nav-menu');
-  const navActions = document.querySelector('.nav-actions');
+ 
 
   hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
